3343c69401e112cfa1c1289a9371395435f15b00
joelepore <giuseppejoelepore@gmail.com>
2024-11-13 13:25:29 +0100

    feat: add total price calculation

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ const pricePerKm = 0.21;
 const discountUnderage = 20;
 const discountOver65 = 40;
 
+let totalPrice;
+
 const totKm = parseInt(prompt('Inserisci i km da percorrere'));
 const age = parseInt(prompt('Inserisci la tua eta\''));
 
 console.log('Km da percorrere:', totKm);
 console.log("Eta':", age);
+
+totalPrice = totKm * pricePerKm;
+console.log("Il prezzo pieno del tuo biglietto e' di: €", totalPrice.toFixed(2));
