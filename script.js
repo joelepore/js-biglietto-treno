e5922723e6c6f6e8ea7853bdebed81092c986d64
joelepore <giuseppejoelepore@gmail.com>
2024-11-13 13:35:42 +0100

    feat: add discounted price calculation

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const pricePerKm = 0.21;
 const discountUnderage = 20;
 const discountOver65 = 40;
 
-let totalPrice;
+let totalPrice, totalDiscountedPrice, discountPrice;
+let discount = 0;
 
 const totKm = parseInt(prompt('Inserisci i km da percorrere'));
 const age = parseInt(prompt('Inserisci la tua eta\''));
@@ -11,4 +12,16 @@ console.log('Km da percorrere:', totKm);
 console.log("Eta':", age);
 
 totalPrice = totKm * pricePerKm;
-console.log("Il prezzo pieno del tuo biglietto e' di: €", totalPrice.toFixed(2));
+console.log("Prezzo pieno: €", totalPrice.toFixed(2));
+
+if (age < 18) {
+  discount = discountUnderage;
+} else if (age > 65) {
+  discount = discountOver65;
+}
+
+discountPrice = totalPrice * discount / 100;
+console.log(`Hai diritto a uno sconto del ${discount}% pari a € ${discountPrice.toFixed(2)}`);
+
+totalDiscountedPrice = totalPrice - discountPrice;
+console.log("Il prezzo scontato del tuo biglietto e' di: €", totalDiscountedPrice.toFixed(2));
