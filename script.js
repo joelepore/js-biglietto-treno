d4f8ce35f5a7fca672374b5821162d61a5d10f3c
joelepore <giuseppejoelepore@gmail.com>
2024-11-13 13:18:59 +0100

    feat: add input prompts

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const pricePerKm = 0.21;
 const discountUnderage = 20;
 const discountOver65 = 40;
+
+const totKm = parseInt(prompt('Inserisci i km da percorrere'));
+const age = parseInt(prompt('Inserisci la tua eta\''));
+
+console.log('Km da percorrere:', totKm);
+console.log("Eta':", age);
