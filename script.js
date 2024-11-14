fd66fa7026c06937d5a199527debe06e8d93c00c
joelepore <giuseppejoelepore@gmail.com>
2024-11-14 10:02:39 +0100

    fix: fix final output, displaying better output based on the discount

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ console.log('Km da percorrere:', totKm);
 console.log("Eta':", age);
 
 totalPrice = totKm * pricePerKm;
-console.log("Prezzo pieno: €", totalPrice.toFixed(2));
 
 if (age < 18) {
   discount = discountUnderage;
@@ -20,8 +19,12 @@ if (age < 18) {
   discount = discountOver65;
 }
 
-discountPrice = totalPrice * discount / 100;
-console.log(`Hai diritto a uno sconto del ${discount}% pari a € ${discountPrice.toFixed(2)}`);
-
-totalDiscountedPrice = totalPrice - discountPrice;
-console.log("Il prezzo scontato del tuo biglietto e' di: €", totalDiscountedPrice.toFixed(2));
+if (discount != 0) {
+  console.log("Prezzo pieno: €", totalPrice.toFixed(2));
+  discountPrice = totalPrice * discount / 100;
+  console.log(`Hai diritto a uno sconto del ${discount}% pari a € ${discountPrice.toFixed(2)}`);
+  totalDiscountedPrice = totalPrice - discountPrice;
+  console.log("Il prezzo scontato del tuo biglietto e' di: €", totalDiscountedPrice.toFixed(2));
+} else {
+  console.log("Il prezzo del tuo biglietto e' di: €", totalPrice.toFixed(2));
+}
